b4d996decd6e9652364df3310002691207e1c075
kagawagao <kingsongao1221@gmail.com>
2019-03-29 11:34:21 +0800

    fix: fix export name

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from 'rollup-plugin-terser'
 export default {
   input: 'src/index.js',
   output: {
-    name: 'chainLongWebviewSDK',
+    name: 'ChainLongWebView',
     format: 'umd',
     file: 'dist/chainlong-webview-sdk.min.js'
   },
